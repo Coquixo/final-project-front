f7860c5abd1f0f5336f83eeb99d7346d8a0ac6a1
Alex Lopez <coquixo98@gmail.com>
2023-01-03 02:05:20 +0100

    we made an advane, fixing login form, pending register

--- src/components/Auth/LogIn/LogIn.jsx
+++ src/components/Auth/LogIn/LogIn.jsx
@@ -1,21 +1,49 @@
 import React from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 const Login = () => {
-    return (
-        <div className="formBox">
-            <div className="formTitle">Log In!</div>
+    // return (
+    //     <div className="formBox">
+    //         <div className="formTitle">Log In!</div>
 
-            <div className="textDesign">Email</div>
-            <input
-                type="email"
-                className="inputFormDesign"
-                placeholder="example@gmail.com"
-            />
-            <div className="textDesign">Password</div>
-            <input type="password" className="inputFormDesign" />
-            <div className="submitButton">Submit</div>
-            <div className="errorHandlerDesign">Aqui van los errores</div>
-        </div>
+    //         <div className="textDesign">Email</div>
+    //         <input
+    //             type="email"
+    //             className="inputFormDesign"
+    //             placeholder="example@gmail.com"
+    //         />
+    //         <div className="textDesign">Password</div>
+    //         <input type="password" className="inputFormDesign" />
+    //         <Button variant="light" className="submitButton">Submit</Button>{' '}
+    //         <div className="errorHandlerDesign">Aqui van los errores</div>
+    //     </div>
+    // );
+
+    return (
+        <Container className="  formBox ">
+            <Row>
+                <Col className="formTitle mt-4 mx-5 ">LOG IN</Col>
+            </Row>
+            <Row>
+                <Form>
+                    <Form.Group className="my-4 mx-5">
+                        <Row>
+                            <Form.Label>Email</Form.Label>
+                            <Col>
+                                <Form.Control type="email" placeholder="Example@gmail.com" />
+                            </Col>
+                        </Row>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Enter password" />
+                        <Form.Text className="text-danger errorHandlerDesign ">
+                            Aqui van los errores
+                        </Form.Text>
+                    </Form.Group>
+                    <Button className="my-2 submitButton">SUBMIT</Button>
+                </Form>
+            </Row>
+        </Container>
     );
 };
 
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import Login from "../../components/Auth/LogIn/LogIn";
 import Register from "../../components/Auth/Register/Register";
 import "./Home.scss";
@@ -10,19 +11,43 @@ const Home = () => {
         register: "Log an existing account",
     });
 
+    // return (
+
+    //     <div className="homeViewBox">
+    //         <div className="homeTitle">MJÖLNIR CAPITAL</div>
+    //         <div className="homeMainBox">
+    //             <div className="homeFormBox">
+    //                 {loginForm ? <Login /> : <Register />}
+    //             </div>
+    //             <Button className="optionDesign" variant="dark" onClick={() => setloginForm(!loginForm)}>
+    //                 {!loginForm ? message.register : message.login}</Button>
+
+    //         </div>
+    //     </div >
+    // );
+
     return (
-        <div className="homeViewBox">
-            <div className="homeTitle">MJÖLNIR CAPITAL</div>
-            <div className="homeMainBox">
-                <div className="homeFormBox">
+        <Container>
+            <Row>
+                <Col className="homeTitle bg-dark ">MJÖLNIR CAPITAL</Col>
+            </Row>
+            <Row className="homeMainBox ">
+                <Col></Col>
+                <Col className=" bg-light " sm={12}>
                     {loginForm ? <Login /> : <Register />}
-                </div>
-                <div className="optionDesign" onClick={() => setloginForm(!loginForm)}>
-                    {!loginForm ? message.register : message.login}
-                </div>
-            </div>
-            {loginForm}
-        </div>
+                </Col>
+                <Col></Col>
+                <Row>
+                    <Col>
+                        <Button
+                            className="optionDesign mt-5"
+                            onClick={() => setloginForm(!loginForm)}>
+                            {!loginForm ? message.register : message.login}
+                        </Button>
+                    </Col>
+                </Row>
+            </Row>
+        </Container>
     );
 };
 
