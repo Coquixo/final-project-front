f3f144dacd621e81203e9affcd859ca95efee31d
Alex Lopez <coquixo98@gmail.com>
2023-01-05 23:38:22 +0100

    bringing wallet balances successfully

--- src/containers/BalancesView/Balances.jsx
+++ src/containers/BalancesView/Balances.jsx
@@ -6,36 +6,50 @@ import { getWalletBalance } from "../../services/apiCalls";
 import { userData } from "../../services/slices/userSlice";
 
 const Balances = () => {
-    const [balances, setBalances] = useState([]);
+    const [creditBalances, setCreditBalances] = useState([]);
+    const [debitBalances, setDebitBalances] = useState([]);
     const userReduxCredentials = useSelector(userData);
+    const [totalBalance, setTotalBalance] = useState(0);
 
     useEffect(() => {
-        if (balances.length === 0) {
+        if (creditBalances.length === 0) {
             getWalletBalance(userReduxCredentials.credentials.id, 1).then(
                 (balances) => {
-                    setBalances(balances.data);
-                }
-            );
-            getWalletBalance(userReduxCredentials.credentials.id, 2).then(
-                (balances) => {
-                    setBalances(balances.data);
+                    setCreditBalances(balances.data);
                 }
             );
         }
+        if (debitBalances.length === 0) {
+            getWalletBalance(userReduxCredentials.credentials.id, 2).then(
+                (balances) => {
+                    setDebitBalances(balances.data);
+                }
+            );
+        }
+        parseInt(setTotalBalance(creditBalances.balance + debitBalances.balance));
     }, []);
 
     return (
         <div>
             <MyNavBar />
-            <Container>
+            <Container fluid>
+                <Row className="bg-dark text-danger">
+                    <Col>TOTAL BALANCE: {totalBalance}€</Col>
+                </Row>
                 <Row>
-                    <Col>
-                        <div>
-                            <p>{balances.id}</p>
-                            <p>{balances.UserId}</p>
-                            <p>{balances.CardId}</p>
-                            <p>{balances.balance}</p>
-                        </div>
+                    <Col className="bg-dark text-light">
+                        <span className="text-warning">Credit Card</span>
+                        <p>
+                            Your Credit Balance:{" "}
+                            {creditBalances !== null ? creditBalances.balance : "0"}€
+                        </p>
+                    </Col>
+                    <Col className="bg-dark text-light">
+                        <span className="text-warning">Debit Card</span>
+                        <p>
+                            Your Debit Balance:{" "}
+                            {debitBalances !== null ? debitBalances.balance : "0"}€
+                        </p>
                     </Col>
                 </Row>
             </Container>
